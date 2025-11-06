ca1ba96c4cee42376048b3292e23e13d1b9b74bf
jj_han <1976847645@qq.com>
2025-11-06 20:26:38 +0800

    苍穹外卖v0.0

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,25 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 捕获sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result sqlExceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+
+        //1、用户名已存在
+        if(message.contains("Duplicate entry")){
+            String[] strings = message.split(" ");
+            String key = strings[2];
+            return Result.error(key + MessageConstant.USERNAME_ALREADY_EXIST);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
